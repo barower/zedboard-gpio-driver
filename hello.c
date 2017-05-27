640c796d88236cc598f241290d576dbc1ad95603
Bartek Osinski <barower@gmail.com>
2017-05-27 21:53:23 +0100

    POPRAWKA POPRZEDNIEGO COMMITA
    bo nie dodalem :(

--- hello.c
+++ hello.c
@@ -1,4 +1,4 @@
-/*  
+/*
  *  hello-2.c - Demonstrating the module_init() and module_exit() macros.
  *  This is preferred over using init_module() and cleanup_module().
  */
@@ -6,16 +6,17 @@
 #include <linux/kernel.h>	/* Needed for KERN_INFO */
 #include <linux/init.h>		/* Needed for the macros */
 
-static int __init hello_2_init(void)
+static int __init hello_init(void)
 {
+	int res;
 	printk(KERN_INFO "Hello, world 2\n");
 	return 0;
 }
 
-static void __exit hello_2_exit(void)
+static void __exit hello_exit(void)
 {
 	printk(KERN_INFO "Goodbye, world 2\n");
 }
 
-module_init(hello_2_init);
-module_exit(hello_2_exit);
+module_init(hello_init);
+module_exit(hello_exit);
