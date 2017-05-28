1d8d7ad8fe684f1af1513310f3593601a9745e1e
Bartek Osinski <barower@gmail.com>
2017-05-28 13:39:49 +0100

    READ I WRITE POJEDYNCZEGO ZNAKU
    W nastepnym commicie dodam przykladowe programy potrafiace odczytac dany znak

--- hello.c
+++ hello.c
@@ -5,18 +5,148 @@
 #include <linux/module.h>	/* Needed by all modules */
 #include <linux/kernel.h>	/* Needed for KERN_INFO */
 #include <linux/init.h>		/* Needed for the macros */
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/fs.h>
+#include <asm/uaccess.h>
 
-static int __init hello_init(void)
+MODULE_LICENSE("GPL");
+
+#define SUCCESS 0
+#define DEVICE_NAME "giepeio"
+
+// Char bedacy posrednikiem miedzy user space a gpio
+char mediate[2];
+
+// Rzeczy potrzebne dla rejestracji urzadzenia w systemie plikow
+dev_t my_dev=0;
+struct cdev *my_cdev = NULL;
+static struct class *my_class = NULL;
+
+// Handler gdy wywolany jest write z user space
+ssize_t my_write(struct file *filp,
+	const char __user *buf,size_t count, loff_t *off)
 {
-	int res;
-	printk(KERN_INFO "Hello, world 2\n");
-	return 0;
+  /* Sprawdzamy, czy jest miejsce na urządzeniu */
+	// TODO DO ZMIANY, TUTAJ WPISUJEMY TYLKO WARTOSC GPIO
+  count = 1; // Wysylamy tylko jeden znak
+  //if ( (*off)+count >=  BUF_LEN) count = BUF_LEN-(*off);
+  if (count == 0) return -ENOSPC;
+
+  // Tak, ale trzeba to zapisac do pamieci teraz
+  __copy_from_user(mediate,buf,count);
+
+  (*off) += count;
+  return count;
+}	
+
+// Handler gdy wywolany jest read z user space
+ssize_t my_read(struct file *filp,
+	char __user *buf,size_t count, loff_t *off)
+
+{
+ //if ( (*off)+count >=  BUF_LEN) count = BUF_LEN-(*off);
+  if (count == 0) return 0;
+  // Przesylamy tylko jeden znak
+  count = 1;
+  __copy_to_user(buf,mediate,count);
+  (*off) += count;
+  return count;
+}	
+
+// Handler gdy wywolany jest open z user space
+// POWINNO BYC ZABEZPIECZENIE PRZED WIELOKROTNYM OTWIERANIEM!!!
+static int my_open(struct inode *inode, struct file *file)
+{
+  return SUCCESS;
 }
+
+// Handler gdy wywolany jest read z user space
+static int my_release(struct inode *inode, struct file *file)
+{
+  return SUCCESS;
+}
+
+// Struktura przechowujaca informacje o tym handlerze
+struct file_operations fops = {
+  .owner = THIS_MODULE,
+  .read=my_read,
+  .write=my_write,
+  .open=my_open,
+  .release=my_release,
+};
 
 static void __exit hello_exit(void)
 {
-	printk(KERN_INFO "Goodbye, world 2\n");
+	printk(KERN_INFO "Goodbye, world!\n");
+	// Usuwamy urzadzenie z klasy
+	if(my_dev && my_class){
+		device_destroy(my_class, my_dev);
+	}
+	if(my_cdev){
+		cdev_del(my_cdev);
+	}
+	my_cdev=NULL;
+
+	// Zwalniamy numer urzadzenia
+	unregister_chrdev_region(my_dev, 1);
+
+	// Wyrejestrowujemy klase
+	if(my_class){
+		class_destroy(my_class);
+		my_class = NULL;
+	}
 }
+
+// TODO inicjalizacja adresu w pamieci na gpio
+static int __init hello_init(void)
+{
+	int res;
+	printk(KERN_INFO "Hello, world!\n");
+
+	// Tworzymy klase opisujaca nasze urzadzenie - potrzebne dla udev
+
+	my_class = class_create(THIS_MODULE, "my_class");
+	if (IS_ERR(my_class)) {
+		printk(KERN_ERR "Error creating my_class class.\n");
+		res = PTR_ERR(my_class);
+		goto err1;
+	}
+
+	// Alokujemy numer urzadzenia
+	res = alloc_chrdev_region(&my_dev, 0, 1, DEVICE_NAME);
+	if(res){
+		printk(KERN_INFO "<1>Allocation of the device called %s failed\n", DEVICE_NAME);
+		goto err1;
+	}
+	
+	my_cdev = cdev_alloc();
+	if(my_cdev==NULL){
+		printk(KERN_INFO "<1>Allocation of cdev for %s failed\n", DEVICE_NAME);
+		res = -ENODEV;
+		goto err1;
+	}
+
+	my_cdev->ops = &fops;
+	my_cdev->owner = THIS_MODULE;
+
+	// Dodajemy urzadzenie znakowe do systemu
+	res = cdev_add(my_cdev, my_dev, 1);
+	if(res){
+		printk(KERN_ERR "Registration of the device number for %s failed\n", DEVICE_NAME);
+		goto err1;
+	}
+	
+	// Tworzymy nasze urzadzenie
+	device_create(my_class, NULL, my_dev, NULL, "my_dev%d", MINOR(my_dev));
+	printk(KERN_ALERT "Registration is a success. The major device %s is %d", DEVICE_NAME, MAJOR(my_dev));
+	return SUCCESS;
+
+err1:
+	hello_exit();
+	return res;
+}
+
 
 module_init(hello_init);
 module_exit(hello_exit);
--- hello.mod.c
+++ hello.mod.c
@@ -18,7 +18,18 @@ static const struct modversion_info ____versions[]
 __used
 __attribute__((section("__versions"))) = {
 	{ 0x21fac097, __VMLINUX_SYMBOL_STR(module_layout) },
+	{ 0x6238cb4f, __VMLINUX_SYMBOL_STR(device_create) },
+	{ 0xb7174bf0, __VMLINUX_SYMBOL_STR(cdev_add) },
+	{ 0x71ecf5d6, __VMLINUX_SYMBOL_STR(cdev_alloc) },
+	{ 0x29537c9e, __VMLINUX_SYMBOL_STR(alloc_chrdev_region) },
+	{ 0x790f2290, __VMLINUX_SYMBOL_STR(__class_create) },
+	{ 0x5c5a0946, __VMLINUX_SYMBOL_STR(class_destroy) },
+	{ 0x7485e15e, __VMLINUX_SYMBOL_STR(unregister_chrdev_region) },
+	{ 0xf080f8e0, __VMLINUX_SYMBOL_STR(cdev_del) },
+	{ 0xe2b4edad, __VMLINUX_SYMBOL_STR(device_destroy) },
 	{ 0x27e1a049, __VMLINUX_SYMBOL_STR(printk) },
+	{ 0x1226bdb6, __VMLINUX_SYMBOL_STR(ex_handler_default) },
+	{ 0xbdfb6dbb, __VMLINUX_SYMBOL_STR(__fentry__) },
 };
 
 static const char __module_depends[]
